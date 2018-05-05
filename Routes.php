13fb542594eda14f01cf96096e64be40f8ea6ff3
Jersson Rodriguez <38302097+edevop@users.noreply.github.com>
2018-05-05 19:59:05 -0400

    Add files via upload

---
+++ Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+# Setting up routes
+Route::set('about', function() {
+    echo "about us";
+});
+
+Route::set('contact', function() {
+    echo "contact us";
+});
+
+?>
--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
-$title = "etrueques.com";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta name="propeller" content="04e2311d8eb3a013bf6d5d48a905a606">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $title; ?></title>
 
-</head>
-<body>
+require_once('Routes.php');
 
-
-
-    <script type="text/javascript" async>
-
-    </script>    
-</body>
-</html>
-<?php
+# loading classes from classes directory to our index
+function __autoload($class_name) {
+    require_once '/classes/'.$class_name.'.php';
+}
 
 ?>

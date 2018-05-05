b868f90439024a4e42991f2ac31d2b21170ac79d
Jersson Rodriguez <38302097+edevop@users.noreply.github.com>
2018-05-05 19:34:30 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$title = "etrueques.com";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="propeller" content="04e2311d8eb3a013bf6d5d48a905a606">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title; ?></title>
+
+</head>
+<body>
+
+
+
+    <script type="text/javascript" async>
+
+    </script>    
+</body>
+</html>
+<?php
+
+?>

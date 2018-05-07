f0c8975f9f94466cc4c6867f4695a51bb58659ba
Jersson Rodriguez <38302097+edevop@users.noreply.github.com>
2018-05-07 07:36:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ define('CSS_PATH', 'http://127.0.0.1/blogger/public/css/');
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="<?=$actual_url; ?>index.css">
+    <link rel="stylesheet" type="text/css" href="<?=$actual_url; ?>/public/css/index.css">
 
 </head>
 <body>
